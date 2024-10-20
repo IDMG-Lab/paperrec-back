50e96eeddc5d1b85744bd20bfe73c5419b6a1613
HuDan <164509884@qq.com>
2024-10-20 14:31:43 +0800

    增加RBAC权限设计和RBAC权限-JWT

--- api/api.py
+++ api/api.py
@@ -4,15 +4,7 @@
 """
 
 from fastapi import APIRouter
-from api.login import index, login
-from api.test_redis import test_my_redis, test_my_redis_depends
+from api.endpoints import user
 
 api_router = APIRouter(prefix="/api/v1", tags=["api路由"])
-
-api_router.get("/index", tags=["api路由"], summary="注册接口")(index)
-
-api_router.get("/login", tags=["api路由"], summary="登录接口")(login)
-
-api_router.get("/test/my/redis", tags=["api路由"], summary="fastapi的state方式")(test_my_redis)
-
-api_router.get("/test/my/redis/depends", tags=["api路由"], summary="依赖注入方式")(test_my_redis_depends)
+api_router.include_router(user.router, prefix='/admin', tags=["用户管理"])
